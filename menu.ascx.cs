6879c162e19131051c6a01e7b70a676d52dca2db
rupuzhao <51755308+rupuzhao@users.noreply.github.com>
2019-06-12 17:09:11 -0400

    Add files via upload

---
+++ menu.ascx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+public partial class userControl_menu : System.Web.UI.UserControl
+{
+    CommonClass ccObj = new CommonClass();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       
+
+    }
+    protected void lnkbtnfeedback_Click(object sender, EventArgs e)
+    {
+        if (Session["UserName"] == null)
+        {
+            Response.Write(ccObj.MessageBox("Please login！", "Default.aspx"));
+        }
+        else
+        {
+            Response.Redirect("feedback.aspx");
+
+        }
+    }
+    protected void lnkbtnMyWord_Click(object sender, EventArgs e)
+    {
+        if (Session["UserName"] == null)
+        {
+            Response.Write(ccObj.MessageBox("Please login！", "Default.aspx"));
+        }
+        else
+        {
+            Response.Redirect("MyWord.aspx");
+
+        }
+    }
+    protected void lnkbtnOut_Click(object sender, EventArgs e)
+    {
+        if (Session["UserName"] != null)
+        {
+            Session["UserID"] = null; //use ID
+            Session["UsertjID"] = null; 
+            Session["Username"] = null;//user name
+            Response.Write(ccObj.MessageBox("Thank you for ordering！", "Default.aspx"));
+        }
+    }
+}
